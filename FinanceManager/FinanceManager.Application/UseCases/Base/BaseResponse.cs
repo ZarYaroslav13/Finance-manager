3275a05640a206bfe2df852793f5db10bde68b7b
Yark13 <yaroslav.zarytskyi.ki.2021@lpnu.ua>
2025-03-10 10:34:30 +0200

    Added BaseResponse<T>

---
+++ FinanceManager/FinanceManager.Application/UseCases/Base/BaseResponse.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinanceManager.Application.UseCases.Base;
+
+public class BaseResponse<T>
+{
+    public bool Success { get; set; }
+
+    public T? Data { get; set; }
+
+    public string? Message { get; set; }
+
+    public IEnumerable<BaseError>? Errors { get; set; }
+}

6b8f688ec939799eccefe56ae8e5a2f09ebba956
Yark13 <yaroslav.zarytskyi.ki.2021@lpnu.ua>
2025-04-03 16:51:19 +0300

    Deleted Weather related files

---
+++ FinanceManager/FinanceManager.Web/Pages/Home.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FinanceManager.Web.Pages
+{
+    public class HomeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}

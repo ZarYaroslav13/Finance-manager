b4f42e707b3180ec08030503f2b36228746e31f3
Yark13 <yaroslav.zarytskyi.ki.2021@lpnu.ua>
2025-04-21 20:37:11 +0300

    Changed static SSR type of rendering to interactive SSR

--- FinanceManager/FinanceManager.Web/Program.cs
+++ FinanceManager/FinanceManager.Web/Program.cs
@@ -2,44 +2,50 @@ using FinanceManager.Web;
 using FinanceManager.Web.Components;
 using MudBlazor.Services;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add service defaults & Aspire components.
-builder.AddServiceDefaults();
-builder.Services.AddMudServices();
-
-// Add services to the container.
-builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
-
-builder.Services.AddOutputCache();
-
-builder.Services.AddHttpClient<HttpClient>(client =>
-    {
-        // This URL uses "https+http://" to indicate HTTPS is preferred over HTTP.
-        // Learn more about service discovery scheme resolution at https://aka.ms/dotnet/sdschemes.
-        client.BaseAddress = new("https://apiservice");
-    });
-
-var app = builder.Build();
-
-if (!app.Environment.IsDevelopment())
+public class Program
 {
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add service defaults & Aspire components.
+        builder.AddServiceDefaults();
+
+        // Add services to the container.
+        builder.Services.AddRazorComponents()
+            .AddInteractiveServerComponents();
+        builder.Services.AddMudServices();
+
+        builder.Services.AddOutputCache();
+
+        builder.Services.AddHttpClient<HttpClient>(client =>
+            {
+                // This URL uses "https+http://" to indicate HTTPS is preferred over HTTP.
+                // Learn more about service discovery scheme resolution at https://aka.ms/dotnet/sdschemes.
+                client.BaseAddress = new("https://apiservice");
+            });
+
+        var app = builder.Build();
+
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Error", createScopeForErrors: true);
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseStaticFiles();
+        app.UseAntiforgery();
+
+        app.UseOutputCache();
+
+        app.MapRazorComponents<App>()
+            .AddInteractiveServerRenderMode();
+
+        app.MapDefaultEndpoints();
+
+        app.Run();
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseStaticFiles();
-app.UseAntiforgery();
-
-app.UseOutputCache();
-
-app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
-
-app.MapDefaultEndpoints();
-
-app.Run();

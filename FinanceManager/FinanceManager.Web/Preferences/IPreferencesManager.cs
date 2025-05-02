ce181cc511bd521b7c61eb36448016fa73d54996
Yark13 <yaroslav.zarytskyi.ki.2021@lpnu.ua>
2025-05-02 13:51:46 +0300

    Added FinanceManager.Web.Shared.Wrapper Result classes for better handling errors, starter creation Login page, additionnaly added Langugage Selector and IPreferencesManager

---
+++ FinanceManager/FinanceManager.Web/Preferences/IPreferencesManager.cs
@@ -0,0 +1,8 @@
+﻿namespace FinanceManager.Web.Preferences;
+
+public interface IPreferencesManager
+{
+    Task SetPreference<T>(T preference) where T : class;
+
+    Task<T> GetPreference<T>() where T : class;
+}
---
+++ FinanceManager/FinanceManager.Web/Shared/Wrapper/IResult.cs
@@ -0,0 +1,13 @@
+﻿namespace FinanceManager.Web.Shared.Wrapper;
+
+public interface IResult
+{
+    List<string> Messages { get; set; }
+
+    bool Succeeded { get; set; }
+}
+
+public interface IResult<out T> : IResult
+{
+    T Data { get; }
+}

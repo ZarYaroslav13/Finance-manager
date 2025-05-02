1b119ad91a07f971da8455ce2c38023ef5ab904b
Yark13 <yaroslav.zarytskyi.ki.2021@lpnu.ua>
2025-05-02 12:36:37 +0300

    Added FinanceManager.Web.Shared.Constants.Localization

---
+++ FinanceManager/FinanceManager.Web/ViewModels/LoginViewModel.cs
@@ -0,0 +1,13 @@
+﻿using FinanceManager.Application.UseCases.Login.Commands.SignInCommand;
+
+namespace FinanceManager.Web.ViewModels;
+
+public class LoginViewModel
+{
+    public SignInCommand LoginModel { get; set; }
+
+    public async Task SubmitAsync()
+    {
+
+    }
+}
